ee7e1235ab2c38bb44070551508562509aa8085c
Thiago Tetsuya Futenma <tfutenma@hotmail.com>
2020-12-23 23:08:47 -0300

    Carregando e externalizando CSS

---
+++ 08_webpack/dist/webpack.config.dev.js
@@ -0,0 +1,24 @@
+"use strict";
+
+var webpack = require('webpack');
+
+var MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/principal.js',
+  output: {
+    filename: 'principal.js',
+    path: __dirname + '/public'
+  },
+  plugins: [new MiniCssExtractPlugin({
+    filename: "estilo.css"
+  })],
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: [MiniCssExtractPlugin.loader, // 'style-loader',
+      'css-loader']
+    }]
+  }
+};
