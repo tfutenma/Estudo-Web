1de7e0f4ce408376677c69ec6c430a1e7a2e7c7e
Thiago Tetsuya Futenma <tfutenma@hotmail.com>
2020-12-26 23:51:00 -0300

    gulp (Arrumando bug) e webpack (Setando variavel de ambiente producao e dev, minify js)

--- 08_webpack/dist/webpack.config.dev.js
+++ 08_webpack/dist/webpack.config.dev.js
@@ -1,15 +1,27 @@
 "use strict";
 
+var modoDev = process.env.NODE_ENV !== 'production';
+
 var webpack = require('webpack');
 
 var MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
+var UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+var OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
 module.exports = {
-  mode: 'development',
+  mode: modoDev ? 'development' : 'production',
   entry: './src/principal.js',
   output: {
     filename: 'principal.js',
     path: __dirname + '/public'
+  },
+  optimization: {
+    minimizer: [new UglifyJsPlugin({
+      cache: true,
+      parallel: true
+    }), new OptimizeCssAssetsPlugin({})]
   },
   plugins: [new MiniCssExtractPlugin({
     filename: "estilo.css"
