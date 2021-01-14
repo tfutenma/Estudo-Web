01f1623d37bdcaa0ead3842ec6e59d2ea331450a
Thiago Tetsuya Futenma <tfutenma@hotmail.com>
2021-01-14 17:30:37 -0300

    React index.js

---
+++ 12_react/exercicios/src/dist/index.dev.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var _react = _interopRequireDefault(require("react"));
+
+var _reactDom = _interopRequireDefault(require("react-dom"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+_reactDom["default"].render('<h1>React</h1>', document.getElementById('root'));
