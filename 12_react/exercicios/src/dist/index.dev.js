b0b52910a253eb140e6ee48a13d0f80af5662c5d
Thiago Tetsuya Futenma <tfutenma@hotmail.com>
2021-01-14 19:56:25 -0300

    React - primeiros elementos

--- 12_react/exercicios/src/dist/index.dev.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _react = _interopRequireDefault(require("react"));
-
-var _reactDom = _interopRequireDefault(require("react-dom"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-_reactDom["default"].render('<h1>React</h1>', document.getElementById('root'));
--- 12_react/exercicios/src/index.js
+++ 12_react/exercicios/src/index.js
@@ -1,6 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+// import BomDia from './componentes/BomDia'
+
+// import {BoaTarde, BoaNoite} from './componentes/Multiplos'
+import Multi, {BoaNoite} from './componentes/Multiplos'
+
 const elemento = <h1>React 2</h1>
 
-ReactDOM.render(elemento, document.getElementById('root'))
+// ReactDOM.render(<BomDia nome="Guilherme"/>, document.getElementById('root'))
+
+ReactDOM.render(
+<div>
+      <Multi.BoaTarde nome="Ana"/>
+      <BoaNoite nome="Bia"/>
+</div>, document.getElementById('root'))
