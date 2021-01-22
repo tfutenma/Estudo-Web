f7354d1efd47309e43bd6e6076218e82c567cc5f
Thiago Tetsuya Futenma <tfutenma@hotmail.com>
2021-01-22 03:50:07 -0300

    React Componentes Pai e Filho

--- 12_react/exercicios/src/index.js
+++ 12_react/exercicios/src/index.js
@@ -2,25 +2,38 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 // import BomDia from './componentes/BomDia'
-// const elemento = <h1>React 2</h1>
+// ReactDOM.render(<BomDia nome="Guilherme"/>, document.getElementById('root'))
 
 
 // import {BoaTarde, BoaNoite} from './componentes/Multiplos'
-import Multi, {BoaNoite} from './componentes/Multiplos'
-
-import Saudacao from './componentes/Saudacao'
-
-
-// ReactDOM.render(<BomDia nome="Guilherme"/>, document.getElementById('root'))
-
+//import Multi, {BoaNoite} from './componentes/Multiplos'
 // ReactDOM.render(
 // <div>
 //       <Multi.BoaTarde nome="Ana"/>
 //       <BoaNoite nome="Bia"/>
 // </div>, document.getElementById('root'))
 
+
+//import Saudacao from './componentes/Saudacao'
+// ReactDOM.render(
+//       <div>
+//             <Saudacao tipo="Bom dia" nome="Joao"/>
+//       </div>
+// , document.getElementById('root'))
+
+import Pai from './componentes/Pai'
+import Filho from './componentes/Filho'
 ReactDOM.render(
       <div>
-            <Saudacao tipo="Bom dia" nome="Joao"/>
+            <Pai nome="Paulo" sobrenome="Silva"/>
+            <Pai nome="Paulo" sobrenome="Silva">
+                  <Filho nome="Pedro" sobrenome="Silva"/>
+                  <Filho nome="Paulo" sobrenome="Silva"/>
+                  <Filho nome="Carla" sobrenome="Silva"/>
+            </Pai>
       </div>
 , document.getElementById('root'))
+
+
+
+
